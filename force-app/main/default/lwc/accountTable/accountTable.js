8ad515e7764aa9ceb94a4c5dc02c43d09abce241
suhel-pasha <suhelpashasp1998@gmail.com>
2025-08-02 14:51:37 +0530

    fourth

--- force-app/main/default/lwc/accountTable/accountTable.js
+++ force-app/main/default/lwc/accountTable/accountTable.js
@@ -1,10 +1,16 @@
 import {LightningElement, wire} from 'lwc';
-import getAccounts from '@salesforce/apex/AccountController.getAccounts'
+import getAccounts from '@salesforce/apex/AccountController.getAccounts';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class AccountTable extends LightningElement {
+const actions = [
+   { label: 'View', name: 'view' }
+];
+
+export default class AccountTable extends NavigationMixin(LightningElement) {
 	columns = [
 		{label: 'Name', fieldName: 'Name'},
-		{label: 'Industry', fieldName: 'Industry'}
+		{label: 'Industry', fieldName: 'Industry'},
+		{type: 'action', typeAttributes: { rowActions: actions } }
 	];
 
 	@wire(getAccounts) accounts;
@@ -16,5 +22,20 @@ export default class AccountTable extends LightningElement {
 
 	get noRecords() {
 		return !this.accounts?.data?.length;
+	}
+
+	handleRowAction(event) {
+	const actionName = event.detail.action.name;
+	const row = event.detail.row;
+	if(actionName === 'view') {
+	 this[NavigationMixin.Navigate]({
+                type: 'standard__recordPage',
+                attributes: {
+                    recordId: row.Id,
+                    objectApiName: 'Account',
+                    actionName: 'view'
+                }
+            });	
+	}
 	}
 }
